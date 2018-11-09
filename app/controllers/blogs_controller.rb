9d0f046d5f44e8ad27ca7be7f3d0bd40bceeee98
Ko Watanabe <lsbean82@gmail.com>
2018-11-10 07:13:29 +0900

    Edit blogs controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 class BlogsController < ApplicationController
 
   def index
-    @blogs = Blog.all
+    @blogs = Blog.includes(:user).order("created_at DESC").page(params[:page]).per(10)
   end
 
   def new
@@ -9,9 +9,27 @@ class BlogsController < ApplicationController
   end
 
   def create
-    Blog.create(blog_params)
-
+    @blog = Blog.create(blog_params)
+    redirect_to controller: :blogs, action: :index
   end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+      if @blog.user_id == current_user.id
+        @blog.destroy
+      end
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+      if @blog.user_id == current_user.id
+        @blog.update(blog_params)
+      end
+    end
 
   private
 
